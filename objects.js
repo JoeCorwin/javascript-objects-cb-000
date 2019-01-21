39532b031b91d54eb38de4c4f4be1ceb6727a822
Joe Corwin <joe.corwin@gmail.com>
2019-01-21 03:23:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,15 +1,28 @@
 var playlist = {van_halen: 'jump'}
 
+<<<<<<< HEAD
 
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
+=======
+var = updatePlaylist(playlist, artistName, songTitle) {
+  playlist.artistName = [songTitle]
+>>>>>>> 696cd48779aa794efa102a6a90ac07f146cd6830
 
   return playlist
 }
 
+<<<<<<< HEAD
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
 
   return playlist
 }
+=======
+// // destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+//   obj[key] = value
+//
+//   return obj
+// }
+>>>>>>> 696cd48779aa794efa102a6a90ac07f146cd6830
